e908834da77b1722a842ce09494db47183b087b4
Chris Flatley <chris@committed.io>
2018-02-03 15:12:46 +0000

    Rename module-info to annot8.core, but with suppress warning (as its not a version)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -41,7 +41,8 @@
  * processing on it. However, the pipeline concept is not defined in this module so as to leave
  * users free to combine the components in any manner they wish. 
  */
-module io_annot8_core {
+@SuppressWarnings("module")
+module annot8.core {
   exports io.annot8.core.annotations;
   exports io.annot8.core.bounds;
   exports io.annot8.core.components;
