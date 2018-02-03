fb643859738ffacbb2c0b327bef25ed407ffbea5
Chris Flatley <chris@committed.io>
2018-02-03 14:37:44 +0000

    Remove capabilities implementation defaults

--- src/main/java/io/annot8/core/components/Capabilities.java
+++ src/main/java/io/annot8/core/components/Capabilities.java
@@ -11,17 +11,13 @@ public interface Capabilities {
    * Return the type of any required input annotations (i.e. annotations that
    * must be present before a component can work)
    */
-  default Stream<String> getRequiredInputAnnotations(){
-    return Stream.empty();
-  }
+  Stream<String> getRequiredInputAnnotations();
 
   /**
    * Return the type of any optional input annotations (i.e. annotations that
    * should be present if possible before a component can work)
    */
-  default Stream<String> getOptionalInputAnnotations(){
-    return Stream.empty();
-  }
+  Stream<String> getOptionalInputAnnotations();
 
   /**
    * Return the type of any output annotations produced by this component
@@ -32,22 +28,16 @@ public interface Capabilities {
    * Return the content tags that this component will act on, or an empty
    * stream if this component will act on all content
    */
-  default Stream<String> getAcceptedTags(){
-    return Stream.empty();
-  }
+  Stream<String> getAcceptedTags();
 
   /**
    * Return the content classes produced by this component, or an empty
    * stream if no new content will be produced
    */
-  default Stream<Class<? extends Content<?>>> getCreatedContent(){
-    return Stream.empty();
-  }
+  Stream<Class<? extends Content<?>>> getCreatedContent();
 
   /**
    * Return the resource classes required by this component
    */
-  default Stream<Class<? extends Resource>> getRequiredResources(){
-    return Stream.empty();
-  }
+  Stream<Class<? extends Resource>> getRequiredResources();
 }
