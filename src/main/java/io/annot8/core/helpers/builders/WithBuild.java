7d9e24bfc4de661298112e000b781d9bea3da78e
Chris Flatley <chris@committed.io>
2018-02-05 22:22:46 +0000

    Drop save from stores, adding to builder
    Correct naming of some parameters
    Fix bug in newId()

--- src/main/java/io/annot8/core/helpers/builders/WithBuild.java
+++ src/main/java/io/annot8/core/helpers/builders/WithBuild.java
@@ -3,7 +3,7 @@ package io.annot8.core.helpers.builders;
 import io.annot8.core.exceptions.IncompleteException;
 
 /**
- * Helper interface to indicate that a builder has a build method
+ * Helper interface to indicate that a builder has a save method
  *
  * @param <A> The object being built by this builder
  */
@@ -12,5 +12,5 @@ public interface WithBuild<A> {
   /**
    * Return the object being built by this builder
    */
-  A build() throws IncompleteException;
+  A save() throws IncompleteException;
 }
