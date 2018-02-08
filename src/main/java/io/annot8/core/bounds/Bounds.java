f10216edd8960c37ca3d62d8063eea95b02818a3
Chris Flatley <chris@committed.io>
2018-02-08 13:40:44 +0000

    Add function to bounds to get the covered data

--- src/main/java/io/annot8/core/bounds/Bounds.java
+++ src/main/java/io/annot8/core/bounds/Bounds.java
@@ -1,8 +1,19 @@
 package io.annot8.core.bounds;
 
+import java.util.Optional;
+
 /**
  * Base bounds interface from which all other bounds extend.
  */
 public interface Bounds {
 
+
+  /**
+   * Get the subset of data which is covered by these bounds.
+   *
+   * Most bounds will likely support only one (or very few) required class and data class.
+   *
+   * Return optional empty if their is nothing covered, or if the combinations are not supported.
+   */
+  <D, R> Optional<R> getData(D data, Class<R> requiredClass);
 }
