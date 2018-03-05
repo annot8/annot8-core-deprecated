2684472ab8aa9c76b765538d84e3a3d3c0e32fdb
James Baker <james.d.baker@gmail.com>
2018-03-05 10:23:55 +0000

    Rename DefaultSettings to EmptySettings, and make singleton

--- src/main/java/io/annot8/core/settings/DefaultSettings.java
+++
@@ -1,9 +0,0 @@
-package io.annot8.core.settings;
-
-/**
- * An empty Settings class, used when querying capabilities to indicate to a component that the
- * default settings should be used.
- */
-public final class DefaultSettings implements Settings {
-
-}
---
+++ src/main/java/io/annot8/core/settings/EmptySettings.java
@@ -0,0 +1,16 @@
+package io.annot8.core.settings;
+
+/**
+ * An empty Settings class.
+ */
+public final class EmptySettings implements Settings {
+  private static final EmptySettings INSTANCE = new EmptySettings();
+
+  private EmptySettings(){
+    //No public constructor for singleton
+  }
+
+  public static EmptySettings getInstance(){
+    return INSTANCE;
+  }
+}
