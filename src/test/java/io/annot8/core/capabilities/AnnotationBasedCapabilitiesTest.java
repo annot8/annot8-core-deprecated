833c65b0428a709a95fa6b1e568cf1c55992662e
Chris Flatley <chris@committed.io>
2018-08-09 13:11:03 +0200

    Add tests for annotationbasedcapability - a test will fail!

---
+++ src/test/java/io/annot8/core/capabilities/AnnotationBasedCapabilitiesTest.java
@@ -0,0 +1,161 @@
+package io.annot8.core.capabilities;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import io.annot8.core.bounds.Bounds;
+import io.annot8.core.components.Annot8Component;
+import io.annot8.core.components.Resource;
+import io.annot8.core.data.Content;
+import io.annot8.core.properties.ImmutableProperties;
+import io.annot8.core.stores.AnnotationStore;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AnnotationBasedCapabilitiesTest {
+
+
+  private AnnotationBasedCapabilities capabilities;
+
+  @BeforeEach
+  void beforeEach() {
+    capabilities = new AnnotationBasedCapabilities(AnnotatedComponent.class);
+  }
+
+  @Test
+  void getRequiredInputAnnotations() {
+    assertThat(capabilities.getRequiredInputAnnotations()).containsExactlyInAnyOrder("ar1", "ar2");
+  }
+
+  @Test
+  void getOptionalInputAnnotations() {
+    assertThat(capabilities.getOptionalInputAnnotations()).containsExactlyInAnyOrder("ao1");
+
+  }
+
+  @Test
+  void getOutputAnnotations() {
+    assertThat(capabilities.getOutputAnnotations()).containsExactlyInAnyOrder("a");
+
+  }
+
+  @Test
+  void getRequiredInputGroups() {
+    assertThat(capabilities.getRequiredInputGroups()).containsExactlyInAnyOrder("gr1", "gr2");
+
+  }
+
+  @Test
+  void getOptionalInputGroups() {
+    assertThat(capabilities.getOptionalInputGroups()).containsExactlyInAnyOrder("go1");
+
+  }
+
+  @Test
+  void getOutputGroups() {
+    assertThat(capabilities.getOutputGroups()).containsExactlyInAnyOrder("g");
+
+  }
+
+  @Test
+  void getCreatedContent() {
+    assertThat(capabilities.getCreatedContent()).containsExactlyInAnyOrder(FakeContent.class);
+
+  }
+
+  @Test
+  void getRequiredContent() {
+    assertThat(capabilities.getRequiredContent()).containsExactlyInAnyOrder(FakeContent.class);
+
+  }
+
+  @Test
+  void getRequiredResources() {
+    assertThat(capabilities.getRequiredResources()).containsExactlyInAnyOrder(Resource.class);
+  }
+
+  @Test
+  void getOutputBounds() {
+    assertThat(capabilities.getOutputBounds()).containsExactlyInAnyOrder(FakeBounds.class);
+
+  }
+
+  @Test
+  void getOutputGroupsForChildComponent() {
+    AnnotationBasedCapabilities child = new AnnotationBasedCapabilities(ChildAnnotatedComponent.class);
+    assertThat(child.getOutputGroups()).containsExactlyInAnyOrder("g", "sg");
+  }
+
+
+  @CreatesAnnotations("a")
+  @CreatesBounds(FakeBounds.class)
+  @CreatesContent(FakeContent.class)
+  @CreatesGroups("g")
+  @ProcessesAnnotations(value = "ar1", required = "ar2", optional = "ao1")
+  @ProcessesContent(FakeContent.class)
+  @ProcessesGroups(value = "gr1", required = "gr2", optional = "go1")
+  @UsesResource(Resource.class)
+  public static class AnnotatedComponent implements Annot8Component {
+
+  }
+
+  @CreatesGroups("sg")
+  public static class ChildAnnotatedComponent extends AnnotatedComponent {
+
+  }
+
+  public static class FakeContent implements Content<String> {
+
+    @Override
+    public String getData() {
+      return null;
+    }
+
+    @Override
+    public Class<String> getDataClass() {
+      return null;
+    }
+
+    @Override
+    public Class<? extends Content<String>> getContentClass() {
+      return null;
+    }
+
+    @Override
+    public AnnotationStore getAnnotations() {
+      return null;
+    }
+
+    @Override
+    public String getName() {
+      return null;
+    }
+
+    @Override
+    public String getId() {
+      return null;
+    }
+
+    @Override
+    public ImmutableProperties getProperties() {
+      return null;
+    }
+  }
+
+    public static class FakeBounds implements Bounds {
+
+    @Override
+    public <D, C extends Content<D>, R> Optional<R> getData(C content, Class<R> requiredClass) {
+      return Optional.empty();
+    }
+
+    @Override
+    public <D, C extends Content<D>> boolean isValid(C content) {
+      return false;
+    }
+  }
+}
