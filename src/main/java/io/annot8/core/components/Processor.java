698ed1748c086bba63216746d7b5544472df2ae8
Chris Flatley <chris@committed.io>
2018-10-03 08:14:07 +0100

    Add a WithProcessItem helper for things which 'look like processors' (eg pipelines)

--- src/main/java/io/annot8/core/components/Processor.java
+++ src/main/java/io/annot8/core/components/Processor.java
@@ -1,17 +1,11 @@
 /* Annot8 (annot8.io) - Licensed under Apache-2.0. */
 package io.annot8.core.components;
 
-import io.annot8.core.components.responses.ProcessorResponse;
-import io.annot8.core.data.Item;
-import io.annot8.core.exceptions.Annot8Exception;
+import io.annot8.core.helpers.WithProcessItem;
 
 /**
  * Base processor interface from which all processors extend.
  *
  * <p>Processors do work on an item, such as adding new annotations, or creating new content.
  */
-public interface Processor extends Annot8Component {
-
-  /** Process the given item */
-  ProcessorResponse process(final Item item) throws Annot8Exception;
-}
+public interface Processor extends Annot8Component, WithProcessItem {}
